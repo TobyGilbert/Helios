ef126158d2be7e1bdaa661fc98385813925effa7
Toby Gilbert <tobygilbert@hotmail.co.uk>
2015-01-27 15:47:45 +0000

    spheres added

--- optixSrc/parallelogram.cu
+++ optixSrc/parallelogram.cu
@@ -29,6 +29,8 @@ rtDeclareVariable(float3, v2, , );
 rtDeclareVariable(float3, anchor, , );
 rtDeclareVariable(int, lgt_instance, , ) = {0};
 
+rtDeclareVariable(float4, sphere, , );
+
 rtDeclareVariable(float3, texcoord, attribute texcoord, ); 
 rtDeclareVariable(float3, geometric_normal, attribute geometric_normal, ); 
 rtDeclareVariable(float3, shading_normal, attribute shading_normal, ); 
@@ -37,45 +39,88 @@ rtDeclareVariable(optix::Ray, ray, rtCurrentRay, );
 
 RT_PROGRAM void intersect(int primIdx)
 {
-  float3 n = make_float3( plane );
-  float dt = dot(ray.direction, n );
-  float t = (plane.w - dot(n, ray.origin))/dt;
-  if( t > ray.tmin && t < ray.tmax ) {
-    float3 p = ray.origin + ray.direction * t;
-    float3 vi = p - anchor;
-    float a1 = dot(v1, vi);
-    if(a1 >= 0 && a1 <= 1){
-      float a2 = dot(v2, vi);
-      if(a2 >= 0 && a2 <= 1){
-        if( rtPotentialIntersection( t ) ) {
-          shading_normal = geometric_normal = n;
-          texcoord = make_float3(a1,a2,0);
-          lgt_idx = lgt_instance;
-          rtReportIntersection( 0 );
+    float3 n = make_float3( plane );
+    float dt = dot(ray.direction, n );
+    float t = (plane.w - dot(n, ray.origin))/dt;
+    if( t > ray.tmin && t < ray.tmax ) {
+        float3 p = ray.origin + ray.direction * t;
+        float3 vi = p - anchor;
+        float a1 = dot(v1, vi);
+        if(a1 >= 0 && a1 <= 1){
+            float a2 = dot(v2, vi);
+            if(a2 >= 0 && a2 <= 1){
+                if( rtPotentialIntersection( t ) ) {
+                    shading_normal = geometric_normal = n;
+                    texcoord = make_float3(a1,a2,0);
+                    lgt_idx = lgt_instance;
+                    rtReportIntersection( 0 );
+                }
+            }
         }
-      }
     }
-  }
 }
 
 RT_PROGRAM void bounds (int, float result[6])
 {
-  // v1 and v2 are scaled by 1./length^2.  Rescale back to normal for the bounds computation.
-  const float3 tv1  = v1 / dot( v1, v1 );
-  const float3 tv2  = v2 / dot( v2, v2 );
-  const float3 p00  = anchor;
-  const float3 p01  = anchor + tv1;
-  const float3 p10  = anchor + tv2;
-  const float3 p11  = anchor + tv1 + tv2;
-  const float  area = length(cross(tv1, tv2));
+    // v1 and v2 are scaled by 1./length^2.  Rescale back to normal for the bounds computation.
+    const float3 tv1  = v1 / dot( v1, v1 );
+    const float3 tv2  = v2 / dot( v2, v2 );
+    const float3 p00  = anchor;
+    const float3 p01  = anchor + tv1;
+    const float3 p10  = anchor + tv2;
+    const float3 p11  = anchor + tv1 + tv2;
+    const float  area = length(cross(tv1, tv2));
   
-  optix::Aabb* aabb = (optix::Aabb*)result;
+    optix::Aabb* aabb = (optix::Aabb*)result;
   
-  if(area > 0.0f && !isinf(area)) {
-    aabb->m_min = fminf( fminf( p00, p01 ), fminf( p10, p11 ) );
-    aabb->m_max = fmaxf( fmaxf( p00, p01 ), fmaxf( p10, p11 ) );
-  } else {
-    aabb->invalidate();
-  }
+    if(area > 0.0f && !isinf(area)) {
+        aabb->m_min = fminf( fminf( p00, p01 ), fminf( p10, p11 ) );
+        aabb->m_max = fmaxf( fmaxf( p00, p01 ), fmaxf( p10, p11 ) );
+    } else {
+        aabb->invalidate();
+    }
+}
+
+RT_PROGRAM void intersect_sphere(int primIdx){
+    float3 center = make_float3( sphere.x, sphere.y, sphere.z );
+    float radius = sphere.w;
+    float3 O = ray.origin - center;
+    float b = dot( O, ray.direction );
+    float c = dot( O, O ) - radius*radius;
+    float disc = b*b - c;
+    if( disc > 0.0f ) {
+        float sdisc = sqrtf( disc );
+        float root1 = (-b - sdisc);
+        bool check_second = true;
+        if( rtPotentialIntersection( root1 ) ) {
+            shading_normal = geometric_normal = (O + root1*ray.direction) / radius;
+            if( rtReportIntersection( 0 ) ) check_second = false;
+        }
+        if( check_second ) {
+            float root2 = (-b + sdisc);
+            if( rtPotentialIntersection( root2 ) ) {
+                shading_normal = geometric_normal = (O + root2*ray.direction) / radius;
+                rtReportIntersection( 0 );
+            }
+        }
+    }
+}
+
+RT_PROGRAM void bounds_sphere(int, float result[6])
+{
+    const float area = 2 * (sphere.w * sphere.w);
+    optix::Aabb* aabb = (optix::Aabb*)result;
+
+    float3 center = make_float3(sphere.x,sphere.y,sphere.z);
+    float3 radiusV3 = make_float3(sphere.w,sphere.w,sphere.w);
+    float3 min = center - radiusV3;
+    float3 max = center + radiusV3;
+
+    result[0] = min.x;
+    result[1] = min.y;
+    result[2] = min.z;
+    result[3] = max.x;
+    result[4] = max.y;
+    result[5] = max.z;
 }
 
